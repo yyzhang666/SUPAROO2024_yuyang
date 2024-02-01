2d69d69f4f1c989b22eaa5f184d4980247a75094
yyzhang666 <150924556+yyzhang666@users.noreply.github.com>
2024-02-01 18:52:11 +0000

    submission ROOT_1

---
+++ ROOT2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<fstream>
+
+#include "TFile.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+
+int main(){
+TFile signal("PsuedoData_Histogram_100fb.root");
+TFile background("Background_1fb.root");
+TFile signal_1fb("Signal_1fb.root");
+
+//Creat Canvas
+TCanvas *sig=new TCanvas()
+
+//Create histograms
+TH1D *sig;
+TH1* invariant_mass=new TH1D("mass","invariant mass",100,0.0,200.0);
+
+//Plot the psuedo data
+signal.GetObject("signal",sig);
+sig->Draw();
+
+double event_weight,invariant_mass;
+tree->
+//loop the events
+size_t nEntries=tree->GetEntries();
+for(size_t i=0;i<nEntries;i++){
+    tree->GetEntry(i);
+    
+}
+}
---
+++ ROOT_1.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<fstream>
+
+#include<nlohmann/json.hpp>
+#include<TFile.h>
+#include<TTree.h>
+
+using json = nlohmann::json;
+
+int main(){
+    std::ifstream file("energy_data.json");
+    json data;
+    file>>data;
+
+    TFile rootFile(energy_data.root,"RECREATE");
+    TTree tree("Measurements");
+
+    float cpu,frq,ram,powa,gpu;
+    char timestamp[25];
+
+    tree.Branch("cpu",&cpu);
+    tree.Branch("frq",&frq);
+    tree.Branch("ram", &ram);
+    tree.Branch("powa", &powa);
+    tree.Branch("gpu", &gpu);
+    tree.Branch("timestamp",&timestamp);
+
+    for (auto& entry : data["measurements"].items()) {
+        timestamp = entry.key(); 
+        cpu = entry.value()["cpu"];
+        frq = entry.value()["frq"];
+        ram = entry.value()["ram"];
+        powa = entry.value()["powa"];
+        gpu = entry.value()["gpu"];
+        tree.Fill();
+    }
+
+    tree.Write();
+    rootFile.Close();
+
+    return 0;
+
+}
